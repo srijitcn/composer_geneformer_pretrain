8b943d0d92560b3f02484d31321fdce6a5a1baa4
srijitcn <srijit.nair@databricks.com>
2024-08-21 14:27:07 -0400

    reverted batched

--- create_mds.py
+++ create_mds.py
@@ -1,9 +1,5 @@
 from datasets import load_from_disk
-from streaming import MDSWriter, StreamingDataset
-from transformers.trainer_pt_utils import LengthGroupedSampler
-import pickle
-import torch
-from torch.utils.data import DataLoader
+from streaming import MDSWriter
 
 datadir = "/Geneformer/data"
 dataset_file = f"{datadir}/dataset/genecorpus_30M_2048.dataset"
@@ -27,48 +23,14 @@ train_test_split = dataset.train_test_split(test_size=0.1)
 train_dataset = train_test_split["train"]
 test_dataset = train_test_split["test"]
 
-with open(example_lengths_file, "rb") as f:
-    example_lengths = pickle.load(f)
-
-#Get a sampler
-generator = torch.Generator()
-generator.manual_seed(
-    int(torch.empty((), dtype=torch.int64).random_().item())
-)
-
-sampler = LengthGroupedSampler(
-                dataset=train_dataset,
-                batch_size=geneformer_batch_size,
-                lengths=example_lengths,
-                generator=generator,
-            )
-
-train_dataloader = DataLoader(train_dataset,
-                        shuffle=False, 
-                        drop_last=False, 
-                        sampler=sampler)
-
-#dataset_list = train_dataset.to_pandas().to_dict('records')
+dataset_list = train_dataset.to_pandas().to_dict('records')
 
 # Save the samples as shards using MDSWriter
-#with MDSWriter(out=streaming_dataset_location, columns=columns, compression='zstd') as out:
-##    for x in dataset_list:
-#        out.write({
-#            "input_ids" : x["input_ids"],
-#            "length" : x["length"]
-#        })
-
-with MDSWriter(out=f"{streaming_dataset_location}_batched", columns=columns, compression='zstd') as out:
-    batch_iter = iter(train_dataloader)
-    for i in range(10): # int( len(train_dataset)/geneformer_batch_size ) + 1 ):
-        batch = next(batch_iter)
-        print(batch)
-#        sample = next(sample_iter)
-#        print(sample)
-#        for x in sample:
-#            out.write({
-#                "input_ids" : x["input_ids"],
-#                "length" : x["length"]
-#            })
+with MDSWriter(out=streaming_dataset_location, columns=columns, compression='zstd') as out:
+    for x in dataset_list:
+        out.write({
+            "input_ids" : x["input_ids"],
+            "length" : x["length"]
+        })
 
 
