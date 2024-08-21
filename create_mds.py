3fa8034b94cdd193bdc313283589b288e574d3e2
srijitcn <srijit.nair@databricks.com>
2024-08-21 13:48:27 -0400

    removed subsetting

--- create_mds.py
+++ create_mds.py
@@ -21,7 +21,7 @@ train_test_split = dataset.train_test_split(test_size=0.1)
 train_dataset = train_test_split["train"]
 test_dataset = train_test_split["test"]
 
-dataset_list = train_dataset.select(range(1000)).to_pandas().to_dict('records')
+dataset_list = train_dataset.to_pandas().to_dict('records')
 
 # Save the samples as shards using MDSWriter
 with MDSWriter(out=streaming_dataset_location, columns=columns, compression='zstd') as out:
