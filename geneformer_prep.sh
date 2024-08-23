bca9852c450f65c70cdf159877119fb8d3f8f0cd
srijitcn <srijit.nair@databricks.com>
2024-08-23 13:44:58 -0400

    refactor

--- geneformer_prep.sh
+++ geneformer_prep.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!sh
 #install git-lfs , pre-req for geneformer clone
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 apt-get install git-lfs
