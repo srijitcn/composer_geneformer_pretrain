86a8538e3b6a7ca7412187dffadea3a374245060
srijitcn <srijit.nair@databricks.com>
2024-08-26 16:18:28 -0400

    quiet copy

--- geneformer_prep.sh
+++ geneformer_prep.sh
@@ -8,7 +8,7 @@ cd /
 git clone https://huggingface.co/ctheodoris/Geneformer
 cd Geneformer
 git checkout b07f4b1e8893a0923a8fde223fe3b5a60b976d99
-pip install .
+pip install -q .
 
 #create working dirrectory
 mkdir -p /pretrain
