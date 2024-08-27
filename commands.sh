6dcfb8f5089d625815de8759382a0552542b2a0a
srijitcn <srijit.nair@databricks.com>
2024-08-27 14:00:04 -0400

    download daatset

--- commands.sh
+++ commands.sh
@@ -8,24 +8,25 @@ pip install -q -r requirements.txt
 #create working dirrectory
 mkdir -p /pretrain/temp
 
+sh download_dataset.sh
 ##################################################
 ##the following code is only needed to copy the dataset from s3 to locally create streaming dataset
-echo ">>> Configuring aws"
-cd /  
-apt update
-apt install unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+#echo ">>> Configuring aws"
+#cd /  
+#apt update
+#apt install unzip
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
 
-echo ">>> Copying data from s3.. might take few mins"
-cd /composer_geneformer_pretrain
-mkdir /Geneformer/data/dataset -p 
-aws s3 cp s3://srijit-nair-sandbox-bucket/geneformer/data/token_dictionary.pkl /Geneformer/data/token_dictionary.pkl
-aws s3 cp --recursive s3://srijit-nair-sandbox-bucket/geneformer/data/dataset /Geneformer/data/dataset
+#echo ">>> Copying data from s3.. might take few mins"
+#cd /composer_geneformer_pretrain
+#mkdir /Geneformer/data/dataset -p 
+#aws s3 cp s3://srijit-nair-sandbox-bucket/geneformer/data/token_dictionary.pkl /Geneformer/data/token_dictionary.pkl
+#aws s3 cp --recursive s3://srijit-nair-sandbox-bucket/geneformer/data/dataset /Geneformer/data/dataset
 #mkdir /Geneformer/data -p 
 #aws s3 cp --quiet --recursive s3://srijit-nair-sandbox-bucket/geneformer/data /Geneformer/data
-echo "done"
-python create_mds.py
+#echo "done"
+#python create_mds.py
 
 
