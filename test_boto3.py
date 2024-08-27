602816152e57cc6e110618c7a2b43a61dcdede7e
srijitcn <srijit.nair@databricks.com>
2024-08-27 10:26:07 -0400

    test boto3

---
+++ test_boto3.py
@@ -0,0 +1,6 @@
+import boto3
+import pickle 
+
+s3 = boto3.resource('s3')
+token_dictionary = pickle.load(s3.Bucket("srijit-nair-sandbox-bucket").Object("geneformer/data/token_dictionary.pkl").get()['Body'].read())
+ 
