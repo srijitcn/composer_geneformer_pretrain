f692bcd508ad1b31c6067e88741e3b9518a900c0
yenlow <yenlow@gmail.com>
2024-08-28 14:05:54 -0700

    try inference

--- commands.sh
+++ commands.sh
@@ -1,3 +1,13 @@
+# Setup
+apt update
+apt install unzip nano -y
+
+echo ">>> Configuring aws"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws s3 ls s3://srijit-nair-sandbox-bucket/geneformer/data
+
 echo ">>> Installing Geneformer"
 cd /composer_geneformer_pretrain
 sh geneformer_prep.sh 
