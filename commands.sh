f2035650e0da6d94d864ce30c805e9489f7ae32b
yenlow <yenlow@gmail.com>
2024-08-28 15:26:00 -0700

    update apt install

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 # Setup
-apt update
-apt install --qq unzip nano -y
+apt-get update
+apt-get install unzip nano -y
 
 echo ">>> Configuring aws"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
